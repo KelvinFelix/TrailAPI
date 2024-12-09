37a62da556d0ea5c43adc3a8554a6195173eddeb
KelvinFelix <kelvin.felix2502@outlook.com>
2024-12-09 17:51:09 +0000

    feat: Trail TypeORM entity

---
+++ src/infra/repos/postgres/entities/trail.ts
@@ -0,0 +1,31 @@
+import { TrailKind } from '@/domain/entities'
+
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
+
+@Entity({ name: 'trail' })
+export class Trail {
+  @PrimaryGeneratedColumn()
+  id!: number
+
+  @Column({ type: 'varchar', length: 50 })
+  name!: string
+
+  @Column({ type: 'varchar', length: 200 })
+  description!: string
+
+  @Column({ type: 'real' })
+  length!: number
+
+  @Column({ name: 'elevation_gain', type: 'smallint' })
+  elevationGain!: number
+
+  @Column({ name: 'average_time', type: 'interval' })
+  averageTime!: string
+
+  @Column({
+    type: "enum",
+    enum: TrailKind,
+    default: TrailKind.LOOP
+    })
+  kind!: string
+}

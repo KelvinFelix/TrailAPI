1d61a10bb19f701062b365c700df31a11346f08c
KelvinFelix <kelvin.felix2502@outlook.com>
2025-01-05 18:22:31 +0000

    feat: created required field validator

---
+++ src/application/validation/required.ts
@@ -0,0 +1,15 @@
+import { RequiredFieldError } from '@/application/errors'
+import { Validator } from '@/application/contracts'
+
+export class Required implements Validator {
+  constructor (
+    readonly value: any,
+    readonly fieldName?: string
+  ) {}
+
+  validate (): Error | undefined {
+    if (this.value === null || this.value === undefined) {
+      return new RequiredFieldError(this.fieldName)
+    }
+  }
+}

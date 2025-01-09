998541641a632c55240160c79a0c7d49358dd93f
KelvinFelix <kelvin.felix2502@outlook.com>
2025-01-09 11:37:39 +0000

    feat: server routes setup

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -2,5 +2,6 @@ export const env = {
   weatherDataApi: {
     appid: process.env.WEATHER_DATA_API_APP_ID ?? ''
   },
+  port: process.env.PORT ?? 8080,
   jwtSecret: process.env.JWT_SECRET ?? 'jk43h5jk43h5k34'
 }
